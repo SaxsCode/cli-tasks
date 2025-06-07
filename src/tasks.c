a9901fd930afd431a13edb48058a5620e920eb21
Sax Arch <maxsax.sch@gmail.com>
2025-06-07 12:58:46 +0200

    feat: setup cli function which reads parameters

--- src/tasks.c
+++ src/tasks.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+
 int main(int argc, char *argv[]) {
-    printf("Hello world\n");
+    if (argc > 0)
+    {
+        char *task = argv[1];
+        printf("%s", task);
+    }
 }
