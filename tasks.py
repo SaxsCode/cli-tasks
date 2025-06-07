037b1c3e501a42e24ae7678dc1a6c52de424f2a8
Sax Arch <maxsax.sch@gmail.com>
2025-06-07 18:34:03 +0200

    chore: setup google oauth and api

--- tasks.py
+++ tasks.py
@@ -1,9 +1,55 @@
 import argparse
+import os.path
 
-parser = argparse.ArgumentParser()
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
-parser.add_argument('--task', type=str, required=True)
+SCOPES = ["https://www.googleapis.com/auth/tasks.readonly"]
 
-args = parser.parse_args()
+def main():
+  creds = None
+  if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
 
-print('Task added: ', args.task)
+  try:
+    service = build("tasks", "v1", credentials=creds)
+
+    # Call the Tasks API
+    results = service.tasklists().list(maxResults=10).execute()
+    items = results.get("items", [])
+
+    if not items:
+      print("No task lists found.")
+      return
+
+    print("Task lists:")
+    for item in items:
+      print(f"{item['title']} ({item['id']})")
+  except HttpError as err:
+    print(err)
+
+
+if __name__ == "__main__":
+  main()
+
+# parser = argparse.ArgumentParser()
+#
+# parser.add_argument('--task', type=str, required=True)
+#
+# args = parser.parse_args()
+#
+# print('Task added: ', args.task)
